77266d9762da8151272e2c38e41f4f149e998415
mikatpt <mikatpt@gmail.com>
2020-12-05 00:16:34 -0500

    Added postgres config info, permissions

--- server/db/index.js
+++ server/db/index.js
@@ -1,18 +1,21 @@
 const { Pool } = require('pg');
 
 // Maybe add config here?
-const pool = new Pool();
+const config = {
+  host: 'localhost',
+  user: 'student',
+  password: 'student',
+  database: 'amazonreviews',
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+};
+const pool = new Pool(config);
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
   process.exit(-1);
 });
-
-// const client = new Client();
-
-// const connect = async () => {
-//   await client.connect();
-// };
 
 module.exports = {
   pool,
