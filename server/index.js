ba9d0f611f50c8b89e47f310053cef3127fdd192
mikatpt <mikatpt@gmail.com>
2020-12-07 14:03:47 -0500

    Added env config, new relic

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const { getReviews, postReview, updateReview, deleteReview } = require('./db/models.js');
