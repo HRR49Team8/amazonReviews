6a6e24fef65e11b908ad2a5218835e8d6366ce15
mikatpt <mikatpt@gmail.com>
2020-12-08 22:24:06 -0500

    Added route for loaderIO

--- server/routes/web.js
+++ server/routes/web.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('*/:id', (req, res) => res.sendFile(path.join(__dirname, '../../client/dist/index.html')));
+router.get(`/${process.env.LOADERIO}`, (req, res) => res.sendFile(path.join(__dirname, `/${process.env.LOADERIO}.txt`)));
 
 module.exports = router;
