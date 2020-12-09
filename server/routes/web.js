f98631c0848db19b2a89b44c7bf50245d4722744
mikatpt <mikatpt@gmail.com>
2020-12-08 22:46:15 -0500

    Fixed loaderIO stuff

--- server/routes/web.js
+++ server/routes/web.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const path = require('path');
 
-router.get('*/:id', (req, res) => res.sendFile(path.join(__dirname, '../../client/dist/index.html')));
-router.get(`/${process.env.LOADERIO}`, (req, res) => res.sendFile(path.join(__dirname, `/${process.env.LOADERIO}.txt`)));
+router.get(`/${process.env.LOADERIO}.txt`, (req, res) => res.sendFile(path.join(__dirname, `/${process.env.LOADERIO}.txt`)));
+router.get('/:id', (req, res) => res.sendFile(path.join(__dirname, '../../client/dist/index.html')));
 
 module.exports = router;
